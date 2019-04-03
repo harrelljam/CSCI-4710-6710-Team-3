f751ea4255376b3bee127591f7c8c586a08dd911
vilkomira19@students.ecu.edu <Wvurbiz1@>
2019-04-03 18:33:58 -0400

    upload+verify done

--- Assn6/main.py
+++ Assn6/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, g, session
 from werkzeug.utils import secure_filename
 import util
 
@@ -7,6 +7,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = dir_path + '/data/'
 
 app = Flask(__name__)
+app.secret_key = 'myapp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
@@ -15,10 +16,21 @@ def home():
 
 @app.route("/verify",  methods=['GET', 'POST'])
 def verify():
-    return render_template("verify.html")
+		#print(session.get('filename', None))
+		if 'filename' not in session:
+		 log = 'no file field in request.'
+		 return render_template('fail.html', log = log)
+		else:
+			filename = secure_filename(session.get('filename', None))
+			column_names, data_part = util.preview_csv(app.config['UPLOAD_FOLDER']+filename)
+			return render_template('verify.html',column_names=column_names, data_part=data_part)
+
 
 @app.route('/api/post_csv', methods=['POST'])
 def post_csv():
+
+	if 'proceed' in request.form:
+		print('yes!')
 	# request.file <class 'werkzeug.datastructures.FileStorage'>
 	# request.url is http://127.0.0.1:5000/api/post_csv
 	# check if the post request has the file part
@@ -42,10 +54,12 @@ def post_csv():
 		# check if the data folder exists, if not create one
 		#print(UPLOAD_FOLDER)
 		if os.path.exists(app.config['UPLOAD_FOLDER']) == False:
-			print("folder does not exist")
+			#print("folder does not exist")
 			os.makedirs(app.config['UPLOAD_FOLDER'])
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		return render_template('verify.html',filename=filename)
+		g.filename=filename
+		session['filename'] = filename
+		return render_template('index.html',filename=filename)
 	print("Not CSV")
 	log = 'Not CSV.'
 	#return False
--- Assn6/util.py
+++ Assn6/util.py
@@ -1,4 +1,6 @@
 # if a file extension is not listed, the system will not upload the file
+import pandas as pd
+
 ALLOWED_EXTENSIONS = set(['csv'])
 
 def allowed_file(filename):
@@ -8,3 +10,22 @@ def allowed_file(filename):
 	return False
     #return '.' in filename and \
      #      filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def preview_csv(filename):
+	'''
+	this function will return a JSON file
+	containing the first 5 lines of a CSV file
+	'''
+	# read csv file
+	df = pd.read_csv(filename)
+	# this will return the total length of your CSV file
+	total_len = len(df)
+	csv_values = []
+	# if length is less than 5
+	if total_len < 5:
+		csv_values = df.values.tolist()
+	else:
+		csv_values = df.head(n=5).values.tolist()
+
+	# grad column names
+	col_names = df.columns.tolist()
+	return col_names, csv_values
